94e957b2259ddb4477fe7420c7ed7b65a6e33e75
Elvis Franco <116116251+TecGato@users.noreply.github.com>
2023-12-12 14:29:18 -0400

    Add React integration and components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), react()]
 });
