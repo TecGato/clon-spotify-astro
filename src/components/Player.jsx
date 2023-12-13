448951aa63bbd37499a2057cbbc02318028511fb
Elvis Franco <116116251+TecGato@users.noreply.github.com>
2023-12-13 14:17:27 -0400

    Add volume control to Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -79,6 +79,7 @@ export function Player() {
     (state) => state
   );
   const audioRef = useRef();
+  const volumeRef = useRef(1);
 
   useEffect(() => {
     if (isPlaying) {
@@ -93,6 +94,7 @@ export function Player() {
     if (song) {
       const src = `/music/${playlist.id}/0${song.id}.mp3`;
       audioRef.current.src = src;
+      audioRef.current.volume = volumeRef.current;
       audioRef.current.play();
     }
   }, [currentMusic]);
@@ -123,7 +125,9 @@ export function Player() {
           className="w-[95px]"
           onValueChange={(value) => {
             const [newVolume] = value;
-            audioRef.current.volume = newVolume / 100;
+            const volumeValue = newVolume / 100;
+            volumeRef.current = volumeValue;
+            audioRef.current.volume = volumeValue;
           }}
         />
       </div>
