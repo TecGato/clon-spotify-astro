c1694c43a749c5b1ac8cf6950f8c4dca523fd9b6
Elvis Franco <116116251+TecGato@users.noreply.github.com>
2023-12-13 13:55:10 -0400

    Add @radix-ui/react-slider and clsx dependencies, and update Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -58,29 +58,53 @@ export const Volume = () => (
   </svg>
 );
 
+export const CurrentSong = ({ image, title, artists }) => {
+  return (
+    <div className={`flex items-center gap-5 relative overflow-hidden`}>
+      <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden">
+        <img src={image} alt={title} />
+      </picture>
+
+      <div className="flex flex-col">
+        <h3 className=" font-semibold text-sm block">{title}</h3>
+        <span className="text-xs opacity-80">{artists?.join(', ')}</span>
+      </div>
+    </div>
+  );
+};
+
 export function Player() {
-  const { isPlaying, setIsPlaying } = usePlayerStore((state) => state);
-  const [CurrentSong, setCurrentSong] = useState(null);
+  const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(
+    (state) => state
+  );
   const audioRef = useRef();
 
   useEffect(() => {
-    audioRef.current.src = `/music/1/01.mp3`;
-  }, []);
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const { song, playlist, songs } = currentMusic;
+    if (song) {
+      const src = `/music/${playlist.id}/0${song.id}.mp3`;
+      audioRef.current.src = src;
+      audioRef.current.play();
+    }
+  }, [currentMusic]);
 
   const handleClick = () => {
-    if (isPlaying) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
-      audioRef.current.volume = 0.1;
-    }
-
     setIsPlaying(!isPlaying);
   };
 
   return (
     <div className="flex flex-row justify-between w-full px-4 z-50">
-      <div>CurrentSong</div>
+      <div>
+        <CurrentSong {...currentMusic.song} />
+      </div>
 
       <div className=" grid place-content-center gap-4 flex-1">
         <div className="flex justify-center">
