7b7f6aad1ae0889cdec69f4bd89671cbe8c66f8b
Elvis Franco <116116251+TecGato@users.noreply.github.com>
2023-12-18 18:53:03 -0400

    Add size prop to CardPlayButton component

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,7 +1,7 @@
 import { Pause, Play } from './Player';
 import { usePlayerStore } from '@/store/playerStore';
 
-export function CardPlayButton({ id }) {
+export function CardPlayButton({ id, size = 'small' }) {
   const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state);
 
@@ -26,12 +26,18 @@ export function CardPlayButton({ id }) {
       });
   };
 
+  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-5 h-5';
+
   return (
     <button
-      className="card-play-button rounded-full bg-green-500 p-4"
+      className="card-play-button rounded-full bg-green-500 p-4 hover:scale-[1.105] transition hover:bg-green-400"
       onClick={handleClick}
     >
-      {isPlayingPlayList ? <Pause /> : <Play />}
+      {isPlayingPlayList ? (
+        <Pause className={iconClassName} />
+      ) : (
+        <Play className={iconClassName} />
+      )}
     </button>
   );
 }
