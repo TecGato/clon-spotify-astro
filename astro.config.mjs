14d241d1cff47503f196a6a2dd79fa056d4167de
Elvis Franco <116116251+TecGato@users.noreply.github.com>
2023-12-18 19:36:47 -0400

    Add Astro Node integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), svelte(), react()]
+  integrations: [tailwind(), svelte(), react()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
