ca898d49e34673c76e9b34bff95e37f690734ea0
Elvis Franco <116116251+TecGato@users.noreply.github.com>
2023-12-18 20:06:42 -0400

    Add @astrojs/vercel package and update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), svelte(), react()],
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
